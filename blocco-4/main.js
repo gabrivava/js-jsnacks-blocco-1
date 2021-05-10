e70a513d41e5b3e3d803e6134a68b625c9716ff3
gabrivava <gabriele.vavassori4@gmail.com>
2021-05-10 11:44:03 +0200

    snack 2 A

--- blocco-4/main.js
+++ blocco-4/main.js
@@ -1,7 +1,15 @@
+// snack 1A
+
 // creare un oggetto palla ch e abbia le seguenti proprietà
 /* nome = palla
 peso = 10 */
-var palla = {
+var Palla = {
     nome: 'palla',
     peso: 10
 }
+// snak 2A
+
+//attraverso un prompt dare la possibilita all'utente di modicifare il peso della palla
+Palla.nome = prompt('inserisci nome palla');
+Palla.peso = prompt('inserisci il nuovo peso della palla');
+console.log(Palla);
